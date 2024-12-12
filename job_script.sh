8e52ae36a9b108fa035857d13334d9b7f159c976
gilberty005 <102889119+gilberty005@users.noreply.github.com>
2024-12-12 14:58:37 -0500

    fix batch script

--- job_script.sh
+++ job_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --mem=256G
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=24
 #SBATCH --gpus-per-node=8
 #SBATCH --account pmg
+
+source activate pytorch_env
 
 python sae.py 
